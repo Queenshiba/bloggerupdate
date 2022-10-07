93b5609797831edfca3e92da525a7caf6ef55a23
Queenshiba <haruka0111.2a@gmail.com>
2022-10-07 14:52:21 +0900

    {

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,21 @@
-var mykey = config.MY_KEY;
-var secretkey = config.SECRET_KEY;
+import config from './config';
+
+let BLOG_ID = config.BLOG_ID;
+let API_KEY = config.API_KEYY;
+
+// let url = "https://www.googleapis.com/blogger/v3/blogs/BLOG_ID/posts?key=API_KEY"
+
+("TEST " + config.url);
+// function get_result() {
+//     let BLOG_ID = config.BLOG_ID;
+//     let API_KEY = config.API_KEYY;
+//     var url = "https://www.googleapis.com/blogger/v3/blogs/" + blog_id + "/posts?key=" + api_key;
+//     var response = UrlFetchApp.fetch(url).getContentText();
+//     var obj = JSON.parse(response);
+//     for (var i = 0; i < obj.items.length; i++) {
+//         Logger.log(obj.items[i].selfLink);
+//         Logger.log(obj.items[i].title);
+//     }
+// }
+
+// console.log(get_result())
